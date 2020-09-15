f2f7215d91baa46399816acccc143631dd211a09
Bart Kalanski <bkalanski@gmail.com>
2020-09-15 16:14:03 +0100

    Added search results component

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBV-OBTELpEqYr3_YL7WI9DGZ7HqVWOWEE'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5, 
+        key: KEY
+    }
+})
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import youtube from '../api/youtube'
+import VideoList from './VideoList'
 
 class App extends React.Component {
+    state = { videos: [], selectedVideo: null }
+    onTermSubmit = async term => {
+       const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
+        //Once we get the response we initialise it as state
+        this.setState({ videos: response.data.items })
+    }
+    onVideoSelect = video => {
+        console.log('From the app',video)
+
+    }
     render(){
     return (
     <div className="ui container">
-        <SearchBar/>
-        </div>
+        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+    </div>
         )
     }
 }
