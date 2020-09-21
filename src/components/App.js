d220387ea83f7da281f30cfe9813b282deb14466
Bart Kalanski <bkalanski@gmail.com>
2020-09-21 15:48:37 +0100

    Added default onload video

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../api/youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null }
+
+    componentDidMount(){
+        this.onTermSubmit('liverpool premier league champions')
+    }
+
     onTermSubmit = async term => {
        const response = await youtube.get('/search', {
             params: {
@@ -12,17 +18,29 @@ class App extends React.Component {
             }
         })
         //Once we get the response we initialise it as state
-        this.setState({ videos: response.data.items })
+        this.setState({ 
+            videos: response.data.items,
+        selectedVideo: response.data.items[0] 
+    })
     }
     onVideoSelect = video => {
-        console.log('From the app',video)
+        this.setState({ selectedVideo: video })
 
     }
     render(){
     return (
     <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <div className="ui grid">
+        <div className="ui row">
+            <div className="eleven wide column">
+        <VideoDetail video={this.state.selectedVideo} />
+        </div>
+        <div className="five wide column">
         <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+        </div>
+        </div>
+        </div>
     </div>
         )
     }
